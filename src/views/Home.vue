32a136de257c1f189b81f3663c5aee94ba7f0654
Wessel <itseagle15@gmail.com>
2026-03-30 14:54:18 +0200

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>The W.E Homepage</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,18 @@
+<script setup>
+import projects from '../Projects.json'
+</script>
+
+<template>
+<div>
+  <h1>Projects</h1>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+          {{project.name }}
+      </li>
+    </ul>
+</div>
+</template>
+
+<style scoped>
+
+</style>
